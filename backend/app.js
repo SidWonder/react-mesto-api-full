b73612fd0ad762d36bd02387aaec48527748f070
SidWonder <o.mildzikov@mail.ru>
2021-08-27 23:46:11 +0300

    fix of cors

--- backend/app.js
+++ backend/app.js
@@ -16,28 +16,29 @@ const { createUser } = require('./controllers/createUser');
 
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
-const options = {
-  origin: [
-    'http://localhost:3000',
-    'https://sidwonder.mesto.nomoredomains.club',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Accept'],
-  credentials: true,
-};
+const corsOptions = require('./middlewares/cors');
+// const options = {
+//   origin: [
+//     'http://localhost:3000',
+//     'https://sidwonder.mesto.nomoredomains.club',
+//   ],
+//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+//   allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Accept'],
+//   credentials: true,
+// };
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
-app.use('*', cors(options));
+app.use('*', corsOptions);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
